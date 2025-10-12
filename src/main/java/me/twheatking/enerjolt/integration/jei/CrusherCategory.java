51225ffdbe9d1034bdb192f0c10a3386cf46ded6
TWheatKing <126175972+TWheatKing@users.noreply.github.com>
2025-10-13 00:17:13 +0100

    Update CrusherCategory.java

--- src/main/java/me/twheatking/enerjolt/integration/jei/CrusherCategory.java
+++ src/main/java/me/twheatking/enerjolt/integration/jei/CrusherCategory.java
@@ -6,11 +6,13 @@ import me.twheatking.enerjolt.recipe.CrusherRecipe;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
@@ -21,10 +23,14 @@ public class CrusherCategory implements IRecipeCategory<RecipeHolder<CrusherReci
 
     private final IDrawable background;
     private final IDrawable icon;
+    private final int width;
+    private final int height;
 
     public CrusherCategory(IGuiHelper helper) {
         ResourceLocation texture = EJOLTAPI.id("textures/gui/container/crusher.png");
-        background = helper.createDrawable(texture, 47, 30, 98, 26);
+        this.width = 98;
+        this.height = 26;
+        background = helper.createDrawable(texture, 47, 30, width, height);
 
         icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(EnerjoltBlocks.CRUSHER_ITEM.get()));
     }
@@ -40,13 +46,23 @@ public class CrusherCategory implements IRecipeCategory<RecipeHolder<CrusherReci
     }
 
     @Override
-    public IDrawable getBackground() {
-        return background;
+    public IDrawable getIcon() {
+        return icon;
     }
 
     @Override
-    public IDrawable getIcon() {
-        return icon;
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public void draw(RecipeHolder<CrusherRecipe> recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
+        background.draw(guiGraphics, 0, 0);
     }
 
     @Override

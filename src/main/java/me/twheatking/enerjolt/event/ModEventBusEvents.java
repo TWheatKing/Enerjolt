f80210f463d9586abab433051e2eeb1da3d037af
TWheatKing <126175972+TWheatKing@users.noreply.github.com>
2025-10-13 00:04:40 +0100

    Update ModEventBusEvents.java
    can revert if doesn't work

--- src/main/java/me/twheatking/enerjolt/event/ModEventBusEvents.java
+++ src/main/java/me/twheatking/enerjolt/event/ModEventBusEvents.java
@@ -9,9 +9,7 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
 
-
-
-@EventBusSubscriber(modid = EJOLTAPI.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = EJOLTAPI.MOD_ID)
 public class ModEventBusEvents {
     @SubscribeEvent
     public static void registerLayers(EntityRenderersEvent.RegisterLayerDefinitions event) {
@@ -20,13 +18,6 @@ public class ModEventBusEvents {
 
     @SubscribeEvent
     public static void entityAttributeEvent(EntityAttributeCreationEvent event) {
-        // Replace "ModEntities.EVIL_IRON_ZOMBIE" with whatever your actual entity registration is called
-        // Common patterns:
-
-        event.put(EnerjoltEntityTypes.EVILIRONZOMBIE.get(), EvilIronZombieEntity.createAttributes().build());
-    }
-
-    public static void registerAttributes(EntityAttributeCreationEvent event) {
         event.put(EnerjoltEntityTypes.EVILIRONZOMBIE.get(), EvilIronZombieEntity.createAttributes().build());
     }
 }
